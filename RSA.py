a7a07b0ead4f6c4813bf9e23a044e5d945e9c5f5
Roman Kirillov <romzak@icloud.com>
2022-09-05 22:45:39 +0300

    added functions: get_num_of_string, e_function, decoder

--- RSA.py
+++ RSA.py
@@ -10,9 +10,11 @@ def eiler_function(p: int, q: int):
 
 
 def prime_numbers():
+    """Возвращает список всех простых чисел от a до b"""
+    a, b = 100, 9999
     flag = 1
     primes = []
-    for num in range(100, 100000):
+    for num in range(a, b):
         for i in range(2, int(math.sqrt(num)) + 1):
             if num % i == 0:
                 flag = 0
@@ -24,6 +26,7 @@ def prime_numbers():
 
 
 def nod(a: int, b: int):
+    """получение НОД чисел a и b"""
     if a < b:
         a, b = b, a
     r = a % b
@@ -35,6 +38,7 @@ def nod(a: int, b: int):
 
 
 def extended_euclidean(a: int, b: int):
+    """Улучшенный алгоритм Евклида для получения множителей Безу"""
     if a < b:
         a, b = b, a
     r0, r1 = a, b
@@ -62,15 +66,59 @@ def abonent_a():
     n = p * q
     f = eiler_function(p, q)
     e = random.randint(1, f - 1)
+
     while nod(f, e) != 1:
-        e = random.randint(2, f - 1)
+        e = random.randint(2, 66000)
     nod1, h, d = extended_euclidean(f, e)
-    print(f'{nod1} =  {f} * {h} + {e} * {d}')
-    print(nod(f, e))
-    #TODO 
-
-abonent_a()
+    d = (2 * f + 1) // e  # ???
+    print(f'p = {p}, q = {q}, n = {n}, f = {f}, e = {e}, d = {d}')
+    return e, n, d
 
 
+def decoder(d, n, c):
+    m = (c ** d) % n
+    fileout = open('outputfile.txt', 'a')
+    fileout.write(str(m))
+    fileout.close()
 
+
+def e_function(m: int, e: int, n: int):
+    c = (m ** e) % n
+    return c
+
+
+def get_num_from_string(s, a):
+    sum = 0
+    i = 0
+    num_m = 0
+    for m in s:
+        if sum >= a:
+            break
+        num_m = int(m)
+        sum += num_m
+        if sum >= a:
+            break
+        sum *= 10
+        i += 1
+    sum /= 10
+    sum -= num_m
+    new_str = s[i:]
+    return new_str, sum
+
+
+def main():
+    fileout = open('outputfile.txt', 'w')
+    fileout.close()
+    e, n, d = abonent_a()
+    logn = int(math.log2(n))
+    fileinput = open('inputfile.txt', 'r')
+    s = fileinput.read()
+    print(logn)
+    while s != '':
+        s, c = get_num_from_string(s, logn)
+        decoder(d, n, c)
+    fileinput.close()
+
+
+main()
 
